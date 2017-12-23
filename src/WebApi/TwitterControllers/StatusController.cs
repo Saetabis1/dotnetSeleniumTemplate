e620cf3d607ab546c814efbc9af9e3e83cb53ae5
Saetabis1 <marca.manu2@gmail.com>
2017-12-23 15:26:33 -0300

    added configs for twitter endpoint

--- src/WebApi/TwitterControllers/StatusController.cs
+++ src/WebApi/TwitterControllers/StatusController.cs
@@ -2,9 +2,12 @@ namespace src.Webapi.TwitterControllers
 {
     public class StatusController
     {
+        private static string baseUrl = Configuration.Get["TwitterBaseUrl"];
+        private static string resource = Configuration.Get["TwitterStatusResource"];
+
         public void update(Parameters parameters)
         {
-
+            
         }
     }
 }
